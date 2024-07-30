ec4183719d28016eb1c10492feaa2c4545eec1db
msaufi2325 <msaufi2325@gmail.com>
2024-07-30 23:43:55 +0900

    Add NextUIProvider and update page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from '@nextui-org/react';
 
 export default function Home() {
   return (
     <div>
       To Do List
+      <Button>Click me</Button>
     </div>
   );
 }
