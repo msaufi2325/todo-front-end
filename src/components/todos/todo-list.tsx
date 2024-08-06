468905d19405e9bd685c13d18bdc7140778ff7d0
msaufi2325 <msaufi2325@gmail.com>
2024-08-07 06:46:16 +0900

    Add todo list functionality to TodoList component

--- src/components/todos/todo-list.tsx
+++ src/components/todos/todo-list.tsx
@@ -1,10 +1,59 @@
-import { Button } from '@nextui-org/react';
+'use client';
+import { useEffect, useState } from 'react';
+
+interface Todo {
+  id: number;
+  title: string;
+  details: string;
+  priority: string;
+  completed: boolean;
+}
 
 export default function TodoList() {
+  const [todos, setTodos] = useState([] as Todo[]);
+
+  useEffect(() => {
+    let todoList = [
+      {
+        id: 1,
+        title: "Learn Next.js",
+        details: "Learn Next.js and how to use it to build a web app",
+        priority: "High",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Learn React",
+        details: "Learn React and how to use it to build a web app",
+        priority: "Medium",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Learn Tailwind CSS",
+        details: "Learn Tailwind CSS and how to use it to style a web app",
+        priority: "Low",
+        completed: false,
+      },
+    ];
+
+    setTodos(todoList);
+  }, []);
+
   return (
     <div>
-      To Do List
-      <Button>Click me</Button>
+      <h2 style={{ color: 'blue' }}>To Do</h2>
+      <hr style={{ border: '1px solid black' }} />
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <h3 style={{ fontWeight: 'bold' }}>{todo.title}</h3>
+            <p>{todo.details}</p>
+            <p>Priority: {todo.priority}</p>
+            <p>{todo.completed ? "Completed" : "Not Completed"}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
